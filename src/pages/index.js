877aebf07fb61d7724bfd92a406105aad8178ee6
valerioos <anayelivaleriovelez0@gmail.com>
2022-08-06 20:46:45 -0500

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,8 @@
 export default function Home() {
-    const hola = 'hola :D'
-
     return ( <
         >
-        Hello Next.js <
         <
-        h1 > /h1 < /
-        >
-    )
+        h1 > Hello Next.js < /h1> <
+        />
+    );
 }
